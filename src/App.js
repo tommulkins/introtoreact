2bf95cb60b9e62d5c249a71bd6cadfa01e69d8e6
Tom Mulkins <tom.mulkins@gmail.com>
2020-04-25 16:02:34 -0400

    added props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    { id: "something-important" },
-    React.createElement("h1", {}, "Adopt Me!")
-  );
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Foxy",
+      animal: "Dog",
+      breed: "Shiba Inu"
+    }),
+    React.createElement(Pet, { name: "Isa", animal: "Cat", breed: "Tabby" }),
+    React.createElement(Pet, { name: "Fluffy", animal: "Dog", breed: "Mixed" })
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
