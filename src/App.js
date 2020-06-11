fa015c230ef06f7e935ec59f01f217610bc114da
Tom Mulkins <tom.mulkins@gmail.com>
2020-06-11 09:50:58 -0400

    ssr setup/impl

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { useState, lazy, Suspense } from "react";
-import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import React, { useState } from "react";
+import { Router } from "@reach/router";
 import ThemeContext from "./ThemeContext";
 import NavBar from "./NavBar";
-
-const Details = lazy(() => import("./Details"));
-const SearchParams = lazy(() => import("./SearchParams"));
+import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   const themeHook = useState("peru");
@@ -15,16 +13,14 @@ const App = () => {
       <ThemeContext.Provider value={themeHook}>
         <div>
           <NavBar />
-          <Suspense fallback={<h1>Loading route...</h1>}>
-            <Router>
-              <SearchParams path="/" />
-              <Details path="/details/:id" />
-            </Router>
-          </Suspense>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
 
-render(<App />, document.getElementById("root"));
+export default App;
