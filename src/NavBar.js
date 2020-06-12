67ada8ce211ea591f42047f9901ac11a146e6033
Tom Mulkins <tom.mulkins@gmail.com>
2020-06-12 10:45:01 -0400

    small refactor

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
-
-const color = "pink";
+import colors from "./colors";
 
 const NavBar = () => (
   <header
     css={css`
-      background-color: ${color};
+      background-color: ${colors.primary};
       padding: 15px;
+      margin-bottom: 1rem;
     `}
   >
     <Link to="/">Adopt Me!</Link>
-    <span aria-label="logo" role="img">
+    <span
+      aria-label="logo"
+      role="img"
+      css={css`
+        font-size: 60px;
+      `}
+    >
       🐩
     </span>
   </header>
---
+++ src/colors.js
@@ -0,0 +1,4 @@
+export default {
+  primary: "#D9C148",
+  secondary: "green",
+};
